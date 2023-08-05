81df0cde1417425fdb8fb7bb0e8a5eeed060ef2e
Natalya Belikova <minaevanv@inbox.ru>
2023-08-05 14:01:47 +0300

    deploy backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,12 +1,13 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config({ path: "./.env.deploy"});
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master', DEPLOY_REPOSITORY
 } = process.env;
 
 module.exports = {
   apps: [{
-    name: 'api-service',
+    name: 'mesto',
     script: './dist/app.js',
   }],
 
@@ -16,10 +17,11 @@ module.exports = {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/pblHbKa/web-plus-pm2-deploy.git',
+      repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      //'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': `bash scripts/deployEnv.sh  ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
